ef5943ec946ba386ca2fa6a443fd1b4d4a4fe401
agungricky <51534400+agungricky@users.noreply.github.com>
2022-01-16 14:41:15 +0700

    Workshop international development

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import "./Assets/App.scss"
+
+function App(){
+  return (
+   <div className="container"> 
+   <div className="header">header</div>
+   <div className = "Section">
+  <div className ="Card Section-body-1">1</div>
+  <div className ="Card Section-body-2">2</div>
+  <div className ="Card Section-body-3">3</div>
+  <div className ="Card Section-body-4">4</div>
+  <div className ="Card Section-body-5">5</div>
+  <div className ="Card Section-body-6">6</div>
+  <div className ="Card Section-body-7">7</div>
+  </div>
+  </div>
+  );
+}
+
+export default App;
